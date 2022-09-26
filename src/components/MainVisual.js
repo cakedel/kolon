64816b7c847dd79b0d1d86c3b9b93b2f48babb6b
cakedel <coalgreyy9@gmail.com>
2022-09-27 02:40:13 +0900

    kolon

--- src/components/MainVisual.js
+++ src/components/MainVisual.js
@@ -4,7 +4,7 @@ const MainVisual = () => {
   return (
     <section className='MainVisual'>
       <div className='inner'>
-        <h2>우리 삶 언제나 코오롱 인터스트리</h2>
+        <h2>당신의 일상 속에 코오롱인더스트리가 함께합니다</h2>
         <p>고객으로부터 가장 사랑받는 기업, 코오롱인더스트리는 역사의 모퉁이마다 혁신적인 제품을 통해 고객의 생활을 변화시키며 국민과 함께 성장해왔습니다. </p>
       </div>
     </section>
