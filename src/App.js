d0cef0ab0e87b96ec2b7c2e92adca52726f028b7
cakedel <coalgreyy9@gmail.com>
2022-09-29 03:25:45 +0900

    axios get error

--- src/App.js
+++ src/App.js
@@ -7,24 +7,48 @@ import { Routes, Route } from "react-router-dom";
 import Stock from "./components/Stock";
 import Company from "./components/Company";
 import Business from "./components/Business";
-function App() {
+import { useCallback, useEffect, useState } from "react";
+import { axios } from "axios";
+
+const App = () => {
+  const [data, getData] = useState();
+  const serviceKey = process.env.SERVICE_KEY;
+
+  useEffect(() => {
+    const url = "https://apis.data.go.kr/1160100/service/GetStockSecuritiesInfoService/getStockPriceInfo?serviceKey=H9Z28aMsFmBs58iRwg8%2FmcKOEtKEKRRD5bzlertTgbLMLt2%2FwYspNVmVSZpTJpFxWFIE%2Bxgf%2BCk9GZdOcne5%2FQ%3D%3D&resultType=json&isinCd=KR7120110002"
+    const getStock = async () => {
+
+        const response = await axios.get(url);
+        console.log(response.data)
+        getData(response.data);
+        
+    };
+    getStock();
+    console.log(data)
+  }, []);
+
   return (
     <Wrapper>
       <Header />
-      <main>
-        <Routes>
-          <Route path="/" element={<MainVisual />} />
-          <Route path="/company" element={<Company />} />
-          <Route path="/business" element={<Business />} />
-          <Route path="/stock" element={<Stock />} />
-        </Routes>
-        <Routes>
-          <Route path="/" element={<MainContents />} />
-        </Routes>
-      </main>
+      {data ? (
+        <main>
+          <Routes>
+            <Route path="/" element={<MainVisual />} />
+            <Route path="/company" element={<Company />} />
+            <Route path="/business" element={<Business />} />
+            <Route path="/stock" element={<Stock />} />
+          </Routes>
+          <Routes>
+            <Route path="/" element={<MainContents />} />
+          </Routes>
+        </main>
+      ) : (
+        <div>Now loading</div>
+      )}
+
       <Footer />
     </Wrapper>
   );
-}
+};
 
 export default App;
