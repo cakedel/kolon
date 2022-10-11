1761f8a42e8414ca6c3888e7f95c3adb5d1a8b23
cakedel <coalgreyy9@gmail.com>
2022-10-11 22:03:52 +0900

    stock component coding

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import Wrapper from "./components/Wrapper";
 import MainContents from "./components/MainContents";
 import { Routes, Route } from "react-router-dom";
 import Stock from "./components/Stock";
@@ -11,36 +10,45 @@ import Sustain from "./components/Sustain";
 import Company from "./components/Company";
 
 const App = () => {
-  const serviceKey =
-    "H9Z28aMsFmBs58iRwg8%2FmcKOEtKEKRRD5bzlertTgbLMLt2%2FwYspNVmVSZpTJpFxWFIE%2Bxgf%2BCk9GZdOcne5%2FQ%3D%3D";
+  const GNbar = [
+    { id: 1, tit: "회사정보", link: "/company" },
+    { id: 2, tit: "제품정보", link: "/product" },
+    { id: 3, tit: "지속가능경영", link: "/sustain" },
+    { id: 4, tit: "투자정보", link: "/stock" },
+    { id: 5, tit: "채용정보", link: "/employment" },
+  ];
   const [data, getData] = useState();
   const theday = new Date();
   const year = theday.getFullYear();
   const month = ("0" + (theday.getMonth() + 1)).slice(-2);
   const yesterday = ("0" + (theday.getDate() - 1)).slice(-2);
-  const today = ("0" + (theday.getDate())).slice(-2);
   const yesterdate = year + month + yesterday;
-  const todate = year + month + today;
 
+  const SERVICE_KEY =
+    "H9Z28aMsFmBs58iRwg8%2FmcKOEtKEKRRD5bzlertTgbLMLt2%2FwYspNVmVSZpTJpFxWFIE%2Bxgf%2BCk9GZdOcne5%2FQ%3D%3D";
   useEffect(() => {
-    const url = `https://apis.data.go.kr/1160100/service/GetStockSecuritiesInfoService/getStockPriceInfo?serviceKey=${serviceKey}&resultType=json&isinCd=KR7120110002&mrktCtg=KOSPI&basDt=20220928`;
+    const url = `https://apis.data.go.kr/1160100/service/GetStockSecuritiesInfoService/getStockPriceInfo?serviceKey=${SERVICE_KEY}&resultType=json&isinCd=KR7120110002&mrktCtg=KOSPI&basDt=20221007`;
     const getStock = async () => {
       const response = await axios.get(url);
       getData(response.data);
     };
     getStock();
   }, []);
-  console.log(data);
+
   return (
     <div>
       {data ? (
-          <Routes>
+        <Routes>
+          <Route element={<Layout GNbar={GNbar} />}>
             <Route path="/" element={<MainContents />} />
-            <Route path="/subVisual/1" element={<Company GNbar={GNbar} />} />
-            <Route path="/subVisual/2" element={<Product GNbar={GNbar} />} />
-            <Route path="/subVisual/3" element={<Sustain GNbar={GNbar} />} />
-            <Route path="/subVisual/4" element={<Stock GNbar={GNbar} />} />
-            <Route path="/subVisual/5" element={<Employment GNbar={GNbar} />} />
+            <Route path="/subVisual/1" element={<Company />} />
+            <Route path="/subVisual/2" element={<Product />} />
+            <Route path="/subVisual/3" element={<Sustain />} />
+            <Route
+              path="/subVisual/4"
+              element={<Stock stockData={data} />}
+            />
+            <Route path="/subVisual/5" element={<Employment />} />
           </Route>
         </Routes>
       ) : (
--- src/components/Stock.js
+++ src/components/Stock.js
@@ -1,24 +1,23 @@
 import React from "react";
 
-const Stock = ({ stockData, beforeyesterdate }) => {
-  const kolonStock = stockData.response.body.items.item[0]
+const Stock = ({ stockData }) => {
+  const kolonStock = stockData.response.body.items.item[0];
   console.log(kolonStock);
   const yesterdayStock = kolonStock.clpr;
   const vsStock = kolonStock.vs;
   const stockNum = Number(yesterdayStock);
   const vsStocknum = Number(vsStock);
-  const trquStockNum = Number(kolonStock.trqu)
-  const date = new Date()
-  const day = date.getDate() - 1
-  const year = date.getFullYear()
-  const month = "0" + date.getMonth()
-  const yesterday = `${year}-${month}-${day}`
+  const trquStockNum = Number(kolonStock.trqu);
+  const date = new Date();
+  const day = date.getDate() - 1;
+  const year = date.getFullYear();
+  const month = "0" + date.getMonth();
+  const yesterday = `${year}-${month}-${day}`;
 
   return (
     <section className="Stock section">
-      <h2>주가 정보</h2>
-      <p>주주의 편의를 위해 실시간 주가정보를 제공해드립니다</p>
       <div className="inner">
+        <h3>주주의 편의를 위해 실시간 주가정보를 제공해드립니다</h3>
         <strong>{yesterday} 기준</strong>
         <table className="stocktable">
           <tbody>
@@ -47,6 +46,86 @@ const Stock = ({ stockData, beforeyesterdate }) => {
               <th className="topTitle">거래량</th>
               <tr className="bottomValue">{trquStockNum.toLocaleString()}</tr>
             </td>
+          </tbody>
+        </table>
+      </div>
+      <div className="inner">
+        <h3>배당정보</h3>
+        <table className="divideInfo">
+          <thead>
+            <tr>
+              <td className="kind">구분</td>
+              <td className="sort">주식종류</td>
+              <td className="year 2021">12기(2021)</td>
+              <td className="year 2020">11기(2020)</td>
+              <td className="year 2019">10기(2019)</td>
+              <td className="year 2018">9기(2018)</td>
+              <td className="year 2017">8기(2017)</td>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th className="doubleCloumn">주당현금배당(원)</th>
+              <td>
+                <tr>보통주</tr>
+                <tr>우선주</tr>
+              </td>
+              <td>
+                <tr>1,300</tr>
+                <tr>1,350</tr>
+              </td>
+              <td>
+                <tr>1,000</tr>
+                <tr>1,050</tr>
+              </td>
+              <td>
+                <tr>900</tr>
+                <tr>950</tr>
+              </td>
+              <td>
+                <tr>900</tr>
+                <tr>950</tr>
+              </td>
+              <td>
+                <tr>1,100</tr>
+                <tr>1,150</tr>
+              </td>
+            </tr>
+            <tr>
+              <th className="doubleCloumn">현금배당수익률(%)</th>
+              <td>
+                <tr>보통주</tr>
+                <tr>우선주</tr>
+              </td>
+              <td>
+                <tr>1.8%</tr>
+                <tr>3.8%</tr>
+              </td>
+              <td>
+                <tr>2.5%</tr>
+                <tr>4.7%</tr>
+              </td>
+              <td>
+                <tr>1.8%</tr>
+                <tr>4.3%</tr>
+              </td>
+              <td>
+                <tr>1.7%</tr>
+                <tr>3.8%</tr>
+              </td>
+              <td>
+                <tr>1.4%</tr>
+                <tr>3.4%</tr>
+              </td>
+            </tr>
+            <tr>
+              <th colSpan={2}>현금배당금 총액(백 만원)</th>
+              <td>39,502</td>
+              <td>29,885</td>
+              <td>26,910</td>
+              <td>26,910</td>
+              <td>31,400</td>
+            </tr>
           </tbody>
         </table>
       </div>
